949bbaabd91bf6e5aade49a6891e22f9a1c643e3
Basitweb <basitrajput1011@gmail.com>
2026-05-02 08:27:04 -0700

    Nodemailer Update

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,13 +1,36 @@
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//       family: 4 // 🔥 FORCE IPv4 (IMPORTANT FIX)
+//   });
+
+//   const mailOptions = {
+//     from: `Your App <${process.env.EMAIL_USER}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     html: options.message,
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    family: 4, // 🔥 FORCE IPv4 (IMPORTANT FIX)
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-      family: 4 // 🔥 FORCE IPv4 (IMPORTANT FIX)
   });
 
   const mailOptions = {
