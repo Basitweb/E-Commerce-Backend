a988f77a6b38bfe59fd78fde17a9426caa065ab8
Basitweb <basitrajput1011@gmail.com>
2026-05-02 08:54:20 -0700

    use resend

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,44 +1,12 @@
-// import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-// export const sendEmail = async (options) => {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//       family: 4 // 🔥 FORCE IPv4 (IMPORTANT FIX)
-//   });
-
-//   const mailOptions = {
-//     from: `Your App <${process.env.EMAIL_USER}>`,
-//     to: options.email,
-//     subject: options.subject,
-//     html: options.message,
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// };
-import nodemailer from "nodemailer";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    family: 4, // 🔥 FORCE IPv4 (IMPORTANT FIX)
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: `Your App <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "Your App <onboarding@resend.dev>",  // use this until you add a custom domain
     to: options.email,
     subject: options.subject,
     html: options.message,
-  };
-
-  await transporter.sendMail(mailOptions);
+  });
 };
